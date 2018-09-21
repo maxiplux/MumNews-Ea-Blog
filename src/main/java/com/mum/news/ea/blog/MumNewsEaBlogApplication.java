3735c7dec2be9d94f76be5d20d8da86cb78082ac
Edwin Cobos <alejo740@gmail.com>
2018-09-21 12:13:41 -0500

    First project setup

---
+++ src/main/java/com/mum/news/ea/blog/MumNewsEaBlogApplication.java
@@ -0,0 +1,12 @@
+package com.mum.news.ea.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumNewsEaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumNewsEaBlogApplication.class, args);
+    }
+}
