310800ac5532eaf8586f003fdddee448508b8768
Edwin Cobos <alejo740@gmail.com>
2018-09-25 10:29:02 -0500

    Fixed bug to folder to store images

--- src/main/java/com/mum/news/ea/blog/MumNewsEaBlogApplication.java
+++ src/main/java/com/mum/news/ea/blog/MumNewsEaBlogApplication.java
@@ -5,10 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.io.File;
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class MumNewsEaBlogApplication {
     public static void main(String[] args) {
+        setupProject();
         SpringApplication.run(MumNewsEaBlogApplication.class, args);
+    }
+
+    private static void setupProject() {
+        File file = new File("upload-dir");
+        if (!file.exists()) {
+            if (file.mkdir()) {
+                System.out.println("Directory is created!");
+            } else {
+                System.out.println("Failed to create directory!");
+            }
+        }
     }
 }
