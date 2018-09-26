2479b70655ef0d6211c9457b4385d64631af479c
Edwin Cobos <alejo740@gmail.com>
2018-09-26 17:39:21 -0500

    Fixed bug image access

--- src/main/java/com/mum/news/ea/blog/security/SecurityConfiguration.java
+++ src/main/java/com/mum/news/ea/blog/security/SecurityConfiguration.java
@@ -43,10 +43,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-
                 .antMatchers("/login").permitAll()
                 .antMatchers("/signup").permitAll()
                 .antMatchers("/").permitAll()
+                .antMatchers("/images/*").permitAll()
                 .antMatchers("/*.png").permitAll()
                 .antMatchers("/*.jpg").permitAll()
                 .antMatchers("/*.css").permitAll()
