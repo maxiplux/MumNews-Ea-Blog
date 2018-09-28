b83e2672aa2f705e42a474370f7565f6dd366b29
Juan Francisco Mosquera <maxiplux@gmail.com>
2018-09-28 18:44:47 -0500

    testing images

--- src/main/java/com/mum/news/ea/blog/controllers/CreateArticleController.java
+++ src/main/java/com/mum/news/ea/blog/controllers/CreateArticleController.java
@@ -7,7 +7,10 @@ import com.mum.news.ea.blog.repositories.ArticleDao;
 import com.mum.news.ea.blog.repositories.CategoryDao;
 import com.mum.news.ea.blog.repositories.UserDao;
 import com.mum.news.ea.blog.storage.StorageService;
+
+import org.apache.tomcat.jni.File;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -24,7 +27,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+
 
 /**
  *Created by IntelliJ IDEA.
@@ -47,6 +57,9 @@ public class CreateArticleController {
     UserDao userDao;
 
     private final StorageService storageService;
+    //https://www.baeldung.com/properties-with-spring
+    @Value("${image.path}")
+    private String imagefolder;
 
     @Autowired
     public CreateArticleController(StorageService storageService) {
@@ -73,11 +86,29 @@ public class CreateArticleController {
 
         User user = userDao.findByEmail(auth.getName());
 
-        storageService.store(file);
+        Path imagePath=Paths.get(String.format("%s//%s",Paths.get(this.imagefolder).toFile().getAbsolutePath() , file.getOriginalFilename()) );
+        if (!file.isEmpty())
+        {
+
+
+            try {
+                byte[] bytes = file.getBytes();
+
+                FileOutputStream outputStream = new FileOutputStream(imagePath.toString());
+                outputStream.write(file.getBytes());
+                outputStream.close();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
         redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
 
-        article.setImage("/images/"+file.getOriginalFilename());
+        article.setImage(imagePath.toString());
 
         article.setPublicationDate(LocalDate.now());
         article.setAuthor(user);
